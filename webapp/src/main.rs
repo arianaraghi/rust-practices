361a1b3198918f32c971895efb6b18d5f43272b9
arianaraghi <47050616+arianaraghi@users.noreply.github.com>
2024-07-27 04:29:44 +0000

    Creating a File type, Handling some Errors that may happen

--- webapp/src/main.rs
+++ webapp/src/main.rs
@@ -1,12 +1,20 @@
 #[macro_use] extern crate nickel;
 
 use nickel::Nickel;
+use std::fs::File;
 
+
+
+fn main() {
+    // pt1();
+    pt2a();
+}
+
+// Part1
 fn say_hello() -> &'static str {
     "Hello dear world!"
 }
-
-fn main() {
+fn pt1() {
     let mut server = Nickel::new();
 
     server.utilize(router! {
@@ -17,3 +25,17 @@ fn main() {
 
     server.listen("127.0.0.1:6767");
 }
+
+// Part2
+fn pt2a() {
+    match File::create("foo.txt") {
+        Ok(_) => println!("File created!"),
+        Err(_) => println!("Error: could not create file.")
+    }
+}
+
+
+
+
+
+
