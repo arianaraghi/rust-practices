c2812c0a1151de39bff99f8ee7d30a362c2cbc00
arianaraghi <47050616+arianaraghi@users.noreply.github.com>
2024-07-31 05:14:34 +0000

    Adding Panic function to be called in case of a panic

--- os-in-rust/src/main.rs
+++ os-in-rust/src/main.rs
@@ -2,6 +2,17 @@
 
 #![no_std]
 
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+/// The PanicInfo parameter contains the file and line where 
+/// the panic happened and the optional panic message. 
+/// The function should never return, so it is marked as a 
+/// diverging function by returning the “never” type !.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
 fn main() {
-    println!("Hello, world!");
 }
