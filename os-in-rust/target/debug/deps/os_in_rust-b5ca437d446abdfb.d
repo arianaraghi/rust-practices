b11dcc1499ac1add14c0b36eba765d49b4df3bc2
arianaraghi <47050616+arianaraghi@users.noreply.github.com>
2024-07-31 05:10:21 +0000

    Remove standard OS libraries with `no_std` attribute

---
+++ os-in-rust/target/debug/deps/os_in_rust-b5ca437d446abdfb.d
@@ -0,0 +1,5 @@
+/workspaces/rust-practices/os-in-rust/target/debug/deps/os_in_rust-b5ca437d446abdfb: src/main.rs
+
+/workspaces/rust-practices/os-in-rust/target/debug/deps/os_in_rust-b5ca437d446abdfb.d: src/main.rs
+
+src/main.rs:
