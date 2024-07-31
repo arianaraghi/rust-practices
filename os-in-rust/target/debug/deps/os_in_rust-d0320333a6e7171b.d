4150a0ca4129265d38c8f822a7ae38bca3042fa9
arianaraghi <47050616+arianaraghi@users.noreply.github.com>
2024-07-31 05:32:56 +0000

    Implementing `start` attribute, and overwrite `crt0`

---
+++ os-in-rust/target/debug/deps/os_in_rust-d0320333a6e7171b.d
@@ -0,0 +1,5 @@
+/workspaces/rust-practices/os-in-rust/target/debug/deps/os_in_rust-d0320333a6e7171b: src/main.rs
+
+/workspaces/rust-practices/os-in-rust/target/debug/deps/os_in_rust-d0320333a6e7171b.d: src/main.rs
+
+src/main.rs:
