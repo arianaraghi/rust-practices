2e509fd4857a3fbe5e07ec21e84bf2b841171b65
arianaraghi <47050616+arianaraghi@users.noreply.github.com>
2024-08-03 05:09:14 +0000

    Removing the Rust specifications for OS by adding a JSON file with new conformation.

---
+++ os-in-rust/target/debug/deps/os_in_rust-95ad9b0cc0512928.d
@@ -0,0 +1,5 @@
+/workspaces/rust-practices/os-in-rust/target/debug/deps/libos_in_rust-95ad9b0cc0512928.rmeta: src/main.rs
+
+/workspaces/rust-practices/os-in-rust/target/debug/deps/os_in_rust-95ad9b0cc0512928.d: src/main.rs
+
+src/main.rs:
