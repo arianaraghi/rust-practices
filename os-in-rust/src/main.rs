4a83af3cfa83a0f83033d9f2794465a627905e8d
arianaraghi <47050616+arianaraghi@users.noreply.github.com>
2024-08-03 04:59:32 +0000

    Installing Rust nightly channel

--- os-in-rust/src/main.rs
+++ os-in-rust/src/main.rs
@@ -2,7 +2,7 @@
 
 #![no_std]
 #![no_main]
-#[no_mangle]
+
 
 use core::panic::PanicInfo;
 
@@ -24,6 +24,7 @@ fn panic(_info: &PanicInfo) -> ! {
 /// extern "C" is to tell the compiler that it should use the
 /// C calling convention for this function (instead of the 
 /// unspecified Rust calling convention).
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
     loop {}
 }
