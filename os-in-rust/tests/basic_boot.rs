ba94609dd0814f957c31425a49f02cfe095f8d24
arianaraghi <47050616+arianaraghi@users.noreply.github.com>
2024-09-11 15:43:45 +0000

    Updating basic boot tests

--- os-in-rust/tests/basic_boot.rs
+++ os-in-rust/tests/basic_boot.rs
@@ -1,10 +1,16 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![test_runner(blog_os::test_runner)]
 
 use core::panic::PanicInfo;
+use blog_os::println;
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
@@ -19,5 +25,5 @@ fn test_runner(tests: &[&dyn Fn()]) {
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    loop {}
+    blog_os::test_panic_handler(info)
 }
